1dffd8515eb103e7c19f453f5deb8afba7448660
Bogdan <bicharaa10@gmail.com>
2023-08-01 12:12:21 +0300

    env add

--- pages/chats.js
+++ pages/chats.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../context";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 
 const ChatEngine = dynamic(() =>
-import("react-chat-engine").then((module) => module.ChatEngine)
+  import("react-chat-engine").then((module) => module.ChatEngine)
 );
 const MessageFormSocial = dynamic(() =>
-import("react-chat-engine").then((module) => module.MessageFormSocial)
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
 );
 
 export default function Chats() {
   const { username, secret } = useContext(Context);
-  const [ showChat, setShowChat ] = useState(false);
+  const [showChat, setShowChat] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -22,23 +22,22 @@ export default function Chats() {
   }, []);
 
   useEffect(() => {
-    if (username.lenght === 0 || secret.lenght === 0) router.push("/")
+    if (username.lenght === 0 || secret.lenght === 0) router.push("/");
   });
 
   if (!showChat) return <div />;
 
   return (
     <div className="background">
-     <div className="shadow">
-      <ChatEngine
-        height="calc(100vh - 200px)"
-        projectID="89b9a160-f387-4998-af40-de3d522322a3"
-        userName={username}
-        userSecret={secret}
-        renderNewMessageForm={() => <MessageFormSocial/>}
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 200px)"
+          projectID="process.env.projectID"
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
         />
+      </div>
     </div>
-  </div>
   );
 }
-
